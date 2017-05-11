03cf585dc76c335f300f30a8da1ab0019012ee1b
Adel <adel.othman@live.com>
2017-05-11 13:05:20 +0200

    First Commit

--- app/src/main/java/com/example/android/playsimple/MainActivity.java
+++ app/src/main/java/com/example/android/playsimple/MainActivity.java
@@ -10,6 +10,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
@@ -103,6 +104,20 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void afterTextChanged(Editable s) {
 
+            }
+        });
+
+        search.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (search.getText().toString().toLowerCase().equals("search")) {
+                    search.setFocusable(true);
+                    search.setFocusableInTouchMode(true);
+                    search.setText("");
+                    search.setTextColor(getResources().getColor(R.color.typing));
+                }
+
+                return false;
             }
         });
 
